1cdbcad4ea20bcfbb83b5a852efbaeb086bf4c06
suhas1409 <suhaskbhavsar45@gmail.com>
2026-07-20 20:22:11 +0530

    Fix Leaflet marker icons in production

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss'
-import { AuthContextProvider } from './context/AuthContext.jsx'
-import { SocketContextProvider } from './context/SocketContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.scss";
 
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
+
+// Fix Leaflet marker icons
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
       <SocketContextProvider>
-          <App />
+        <App />
       </SocketContextProvider>
     </AuthContextProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
